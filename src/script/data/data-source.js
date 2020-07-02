2c4398c8b83f46c1153fa2c3cdc6116a9b9c2483
Mohammad Raska <iban.ninja@gmail.com>
2020-07-02 16:58:33 +0700

    using import module

--- src/script/data/data-source.js
+++ src/script/data/data-source.js
@@ -1,3 +1,5 @@
+import clubs from './clubs.js'
+
 class DataSource {
     static searchClub(keyword) {
         return new Promise((resolve, reject) => {
@@ -14,3 +16,5 @@ class DataSource {
 
     };
 }
+
+export default DataSource;
